f24d200cf15f48baca8cb2da06b219e73d1a915b
Thomas <thomas.giang.0812@gmail.com>
2022-06-02 12:23:47 -0500

    Creating mock data through mock.go.

---
+++ mock.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"math/rand"
+	"time"
+)
+
+	
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+
+func RandCityName(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	city := make([]rune, n)
+  for i := range city {
+    city[i] = letterRunes[rand.Intn(len(letterRunes))]
+  }
+  return string(city)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	cityNames := make([]string, 0)
+	cityCount := 1000
+	directions := make([]string, 4)
+	directions[0] = "north"
+	directions[1] = "east"
+	directions[2] = "south"
+	directions[3] = "west"
+
+	filename := "earth1000.txt"
+	f, err := os.Create(filename)
+	check(err)
+	defer f.Close()
+
+	for i := 0; i < cityCount; i ++ {
+		cityNames = append(cityNames, RandCityName(5));
+	}
+
+	for i := 0; i < cityCount; i ++ {
+		s := fmt.Sprint(cityNames[i]);
+		for j := 0; j < 4; j ++ {
+			if rand.Intn(2) % 5 == 0 {
+				continue;
+			}
+			ss := fmt.Sprint(" " + directions[j] + "=" + cityNames[rand.Intn(cityCount)]);
+			s += ss;
+		}
+		s += "\n";
+		f.WriteString(s);
+	}
+}
